eac20fcb9a3cf27061adbe11bdc4e517284ab9c6
Tore <anurtore01@gmail.com>
2023-05-28 09:38:07 +0600

    Initial Commit

---
+++ egovDelivery/egovDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  egovDeliveryApp.swift
+//  egovDelivery
+//
+//  Created by Tore Amangeldy on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct egovDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
